cd079f8d15492c1c09150fb81ef91840ebe3d706
JohnLuetz <johnluetz@utexas.edu>
2020-03-19 08:51:34 -0500

    can pull daily availablity from schedules

--- ShiftClass.py
+++ ShiftClass.py
@@ -1,18 +1,30 @@
 import datetime as dt
 from datetime import timedelta
 
-#shift class
-class calShift:
-    def __init__(self, name, start, end):
-        self.name = name
-        self.start = start
-        self.end = end
-    def __str__(self):
-        return(f'{self.name}: {(self.start).time()}-{(self.end).time()}')
+#Structured as 
+# List Workweek (List of Workdays)
+# contains list of Employees
+# with each Employee containing the name and hours worked that day
+
+
+# #shift class
+# class calShift:
+#     def __init__(self, name, start, end):
+#         self.name = name
+#         self.start = start
+#         self.end = end
+#     def __str__(self):
+#         return(f'{self.name}: {(self.start).time()}-{(self.end).time()}')
 
 #employee weekly class
 class Employee:
-    def __init__(self, name, weekstart, weekend):
+    def __init__(self, name):
         self.name = name
-        self.weekstart = weekstart
-        self.weekend = weekend
+        self.availability_today = 0
+
+class Workday:
+    def __init__(self, date):
+        self.date = date
+        self.employee_list = []
+    
+ 
--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import csv
 import datetime as dt
 from dateutil.parser import parse
+from ShiftClass import Employee, Workday
 
 
-input_file = "3 Feb - Feb 8.csv"
+input_file = "21 Oct - Oct 25.csv"
+year_number = "2019"
 
-months_of_year = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 datetimes_of_week = []
+Workweek = []
+temp_employees = ['Adrian Cantu','Alec Schultz','John Luetzelschwab','Zachary Keller']
 
 with open(input_file) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
@@ -27,19 +30,44 @@ with open(input_file) as csv_file:
     csv_file.seek(0) 
 
     #get all the dates of the week's schedule
-    list_dates = []
     for row in csv_reader:
         try:
-            if any(month in row[0] for month in months_of_year):
-                datetimes_of_week.append((parse(row[0]).replace(year = 2019)))
+            if year_number in row[0]:
+                Workweek.append(Workday((parse(row[0]).replace(year = int(year_number))))) #adds a Workday to the Workweek
+
         except:
             print("UwU")
 
     csv_file.seek(0) 
     
-    for rownum, row in enumerate(csv_reader):        
-        if rownum > avrow:
-            if row[0] == "EER Employees": #end of employee list
+    for day in Workweek:
+        for person in temp_employees:
+            day.employee_list.append(Employee(person)) #adds the employees to look for to the list in Workday
+
+    #set the hours for each person per day
+    for day in Workweek:
+        startSearch = False
+        csv_file.seek(0) 
+        for rownum, row in enumerate(csv_reader):
+            if startSearch and row[0] == 'EER Employees': #hit the end of the employee list, go onto next day
                 break
-            print(f"{row[0]} : {row[colnum]}")
-            
+
+            try:
+                possible_date = parse(row[0]).replace(year = int(year_number))            
+                if day.date == possible_date: #we found the start of the next day's schedule
+                    startSearch = True
+            except: #row[0] is not a date
+                pass
+
+            for person in day.employee_list:
+                if person.name == row[0]: #if a person in employee_list matches, set their daily hours
+                    person.availability_today = row[29]
+        pass
+    
+    for day in Workweek: #print all days and employee hours
+        print(f"\n{day.date}:")
+        for employee in day.employee_list:
+            print(f"{employee.name} : {employee.availability_today}")
+
+
+
